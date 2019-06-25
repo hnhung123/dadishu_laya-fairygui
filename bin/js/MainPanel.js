aa5ee93bff874a810be96b7b02a69fd98aeb421e
Anna <hnhung123@gmail.com>
2019-06-25 17:45:55 +0800

    Add countdown time progressBar

--- bin/js/MainPanel.js
+++ bin/js/MainPanel.js
@@ -3,7 +3,21 @@ var MainPanel = /** @class */ (function () {
         this._view = fairygui.UIPackage.createObject("hitMole", "back").asCom;
         this._view.setSize(fairygui.GRoot.inst.width, fairygui.GRoot.inst.height);
         fairygui.GRoot.inst.addChild(this._view);
+        this._progressBar = this._view.getChild("progressBar").asProgress;
+        this._progressBar.value = 10;
+        Laya.timer.loop(1000, this, this.onLoop);
     }
+    MainPanel.prototype.onLoop = function () {
+        this._progressBar.value -= 1;
+        console.log('countdown time:', this._progressBar.value);
+        if (this._progressBar.value <= 0) {
+            this.gameOver();
+        }
+    };
+    MainPanel.prototype.gameOver = function () {
+        Laya.timer.clear(this, this.onLoop);
+        console.log("GameOver");
+    };
     return MainPanel;
 }());
 //# sourceMappingURL=MainPanel.js.map
--- src/MainPanel.ts
+++ src/MainPanel.ts
@@ -1,8 +1,24 @@
 class MainPanel{
     private _view:fairygui.GComponent;
+    private _progressBar:fairygui.GProgressBar;
     constructor(){
         this._view=fairygui.UIPackage.createObject("hitMole","back").asCom;
         this._view.setSize(fairygui.GRoot.inst.width,fairygui.GRoot.inst.height);
-        fairygui.GRoot.inst.addChild(this._view)
+        fairygui.GRoot.inst.addChild(this._view);
+
+        this._progressBar=this._view.getChild("progressBar").asProgress;
+        this._progressBar.value=10;
+        Laya.timer.loop(1000,this,this.onLoop);
+    }
+    onLoop():void{
+        this._progressBar.value-=1;
+        console.log('countdown time:',this._progressBar.value)
+        if(this._progressBar.value<=0){
+            this.gameOver();
+        }
+    }
+    gameOver():void{
+        Laya.timer.clear(this,this.onLoop);
+        console.log("GameOver")
     }
 }
